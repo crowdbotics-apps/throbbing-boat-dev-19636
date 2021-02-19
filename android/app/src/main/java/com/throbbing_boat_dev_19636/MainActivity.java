84cb6254738b937e8eb3543351a61d7b347ef9c8
Crowdbotics <team@crowdbotics.com>
2021-02-19 21:18:26 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/throbbing_boat_dev_19636/MainActivity.java
@@ -0,0 +1,15 @@
+package com.throbbing_boat_dev_19636;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "throbbing_boat_dev_19636";
+    }
+}
